b26363535dd873a6a07027a617bf827726e1ecbb
Manthan Shekhar Bhoir <68176997+ManthanBhoir2006@users.noreply.github.com>
2020-07-20 12:31:30 +0530

    Add files via upload

---
+++ Day5assignment/shoppinglist.js
@@ -0,0 +1,15 @@
+console.log("This is my shopping list");
+var myList = [
+    ["Oranges",5],
+    ["chocolate",2],
+];
+// console.log(myList);
+// console.log(myList.length);
+myList.push = [
+    ["Wheat",5],
+    ["Rice",5]
+];
+myList.shift = [
+    ["Salt",5]
+];
+console.log(myList);
